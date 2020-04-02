8e683bc0056076907b28d69b647cd1d271f22b29
leocaseiro <leobok@gmail.com>
2020-04-02 16:37:28 +1100

    add layout for 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,20 +1,23 @@
 import React from 'react'
+import Layout from '../components/Layout'
 
 const NotFoundPage = () => (
-	<section className="section section--gradient">
-		<div className="container">
-			<div className="columns">
-				<div className="column is-10 is-offset-1">
-					<div className="section">
-						<h2 className="title is-size-3 has-text-weight-bold is-bold-light">
-							Opa! Esta página não pode ser encontrada.
-						</h2>
-						<div className="content">Parece que não foi encontrado nada neste local.</div>
+	<Layout>
+		<section className="section section--gradient">
+			<div className="container">
+				<div className="columns">
+					<div className="column is-10 is-offset-1">
+						<div className="section">
+							<h2 className="title is-size-3 has-text-weight-bold is-bold-light">
+								Opa! Esta página não pode ser encontrada.
+							</h2>
+							<div className="content">Parece que não foi encontrado nada neste local.</div>
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
-	</section>
+		</section>
+	</Layout>
 );
 
 export default NotFoundPage
