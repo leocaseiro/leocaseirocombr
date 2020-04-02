4df88414ea93554c64a4d9f5c83aad0e97b4d865
leocaseiro <leobok@gmail.com>
2020-04-02 17:30:27 +1100

    chore: add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -89,6 +89,13 @@ module.exports = {
 			},
 		},
 		{
+			resolve: 'gatsby-plugin-google-analytics',
+			options: {
+				trackingId: 'UA-5498390-1',
+				head: false,
+			},
+		},
+		{
 			resolve: 'gatsby-plugin-purgecss', // purges all unused/unreferenced css rules
 			options: {
 				develop: true, // Activates purging in npm run develop
